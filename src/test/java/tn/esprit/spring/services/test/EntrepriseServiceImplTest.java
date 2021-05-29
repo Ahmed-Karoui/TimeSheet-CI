5d9e85d8c291dc9290d6023a88e87605fc068b88
Ahmed Karoui <karouii.ahmed@gmail.com>
2021-05-29 01:39:55 +0100

    fixed all test methods

---
+++ src/test/java/tn/esprit/spring/services/test/EntrepriseServiceImplTest.java
@@ -0,0 +1,54 @@
+package tn.esprit.spring.services.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import tn.esprit.spring.entities.Departement;
+import tn.esprit.spring.entities.Employe;
+import tn.esprit.spring.entities.Entreprise;
+import tn.esprit.spring.services.EmployeServiceImpl;
+import tn.esprit.spring.services.IEmployeService;
+import tn.esprit.spring.services.IEntrepriseService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EntrepriseServiceImplTest {
+	
+private static final Logger logger = LogManager.getLogger(EmployeServiceImpl.class);
+	
+	public EntrepriseServiceImplTest() {
+		logger.info(" Test : Classe EntrepriseServiceImplTest ...");
+	}
+	
+	@Autowired
+	IEntrepriseService entrepriseServiceImpl ;
+	IEmployeService employeServiceImpl;
+	
+	@Test
+	public void testAjouterEmployeSuccess() {
+		logger.info(" Le test de la methode Ajout Entreprise .... ");
+		assertEquals(1, entrepriseServiceImpl.ajouterEntreprise(new Entreprise("TestBouhmid","Bouhmid")));
+					
+	}
+	
+	@Test
+	public void testAjouterEmployeFailure() {
+		logger.info(" Le test de la methode Ajout Entreprise.... ");
+		assertNotEquals(2, entrepriseServiceImpl.ajouterEntreprise(new Entreprise("TestBouhmid","Bouhmid")));
+
+}
+	@Test
+	public void testAjouterDepartement() {
+		logger.info(" Le test de la methode Ajout Departement .... ");
+		assertEquals(11, employeServiceImpl.ajouterDepartement(new Departement("Departement Informatique")));
+	}
+}
